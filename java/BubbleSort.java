c128f930a8c5e7c801dde15ffe5344d62dd4c22e
kblk <84843856+kblkLab@users.noreply.github.com>
2021-10-06 13:12:44 +0700

    Push Bubble Sort Java

---
+++ java/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.bubblewrap;
+class Sort{
+    int[] arr1 = {9,14,3,2,43,11,58,22};
+    void forSort(){
+        int swap;
+        boolean stop=false;
+        while(!stop){
+            stop=true;
+            for(int a=0;arr1.length-2>=a;a++){
+                if (arr1[a] > arr1[a + 1]) {
+                    swap = arr1[a + 1];
+                    arr1[a + 1] = arr1[a];
+                    arr1[a] = swap;
+                    stop = false;
+                }
+            }
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Sort S=new Sort();
+        System.out.print("Unsorted: ");
+        for (int arr: S.arr1) System.out.print(arr+" ");
+        System.out.print("\nSorted: ");
+        S.forSort();
+        for (int arr: S.arr1) System.out.print(arr+" ");
+    }
+}
